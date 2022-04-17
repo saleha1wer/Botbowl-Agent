9790af2d2d9950bb8bfb1b7a9a690b84f559319a
saleha1wer <95382833+saleha1wer@users.noreply.github.com>
2022-04-18 00:05:45 +0200

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -401,15 +401,3 @@ class MCTS_Run():
 
         self.network.save_network('Network_Run{}'.format(run))
         return wr_per_run
-
-
-
-env_conf = EnvConf(size=5)
-env = BotBowlEnv(env_conf,away_agent='human') 
-env.reset()
-# env.render(feature_layers=True)
-env = RewardWrapper(env, home_reward_func=A2C_Reward())
-env.game.enable_forward_model()
-spatial_obs, non_spatial_obs, action_mask = env.get_state() 
-obs = (spatial_obs, non_spatial_obs, action_mask)
-root = Node(env,obs,1)
